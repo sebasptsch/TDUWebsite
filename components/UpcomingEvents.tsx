c9accf680103b698f442f0d75142663575233e42
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-07-30 22:01:36 +1000

    no events scheduled

--- components/UpcomingEvents.tsx
+++ components/UpcomingEvents.tsx
@@ -5,6 +5,7 @@ import useSWR from "swr";
 
 export default function UpcomingComponent() {
   const { data, error } = useSWR("/api/tba/events", fetcher);
+  console.log(data);
   return (
     <motion.div
       className="has-text-centered pt-6 pb-6 columns is-vcentered is-gapless"
@@ -17,29 +18,28 @@ export default function UpcomingComponent() {
         <div className="content">
           <div className="title">Upcoming Events</div>
           <motion.div variants={container} initial="hidden" animate="show">
-            {data ? (
-              data.map((event) => (
-                <motion.a
-                  className="box"
-                  href={`https://www.thebluealliance.com/event/${event.key}`}
-                  key={event.key}
-                  variants={item}
-                >
-                  <p className="subtitle">{event.name}</p>
-                  {event.active ? (
-                    <>
-                      <div className="tag is-primary">Ongoing</div>
-                      <br />
-                    </>
-                  ) : null}
-                  <b>Starts:</b> {new Date(event.start).toLocaleDateString()}
-                  <br />
-                  <b>Ends:</b> {new Date(event.end).toLocaleDateString()}
-                </motion.a>
-              ))
-            ) : (
-              <p className="has-text-centered">Loading</p>
-            )}
+            {data
+              ? data.map((event) => (
+                  <motion.a
+                    className="box"
+                    href={`https://www.thebluealliance.com/event/${event.key}`}
+                    key={event.key}
+                    variants={item}
+                  >
+                    <p className="subtitle">{event.name}</p>
+                    {event.active ? (
+                      <>
+                        <div className="tag is-primary">Ongoing</div>
+                        <br />
+                      </>
+                    ) : null}
+                    <b>Starts:</b> {new Date(event.start).toLocaleDateString()}
+                    <br />
+                    <b>Ends:</b> {new Date(event.end).toLocaleDateString()}
+                  </motion.a>
+                ))
+              : "Loading"}
+            {data?.length === 0 ? "No Events Scheduled" : null}
           </motion.div>
         </div>
       </div>

473466d3fa1cfcc368ab4fa1617d8eec0b91ac1b
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2023-03-18 19:32:59 +1100

    log body

--- src/pages/api/contact.tsx
+++ src/pages/api/contact.tsx
@@ -55,7 +55,7 @@ export default async function ContactApi(
       body: JSON.stringify(body),
     });
 
-    console.error(response);
+    console.error(response, body);
 
     // let response = await axios.post(process.env.WEBHOOK_URL, );
 

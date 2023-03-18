2f68c60958683f2170e04e785eecf144dfc587c7
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2023-03-18 15:45:14 +1100

    styles

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const { withPlaiceholder } = require('@plaiceholder/next')
 /** @type {import('next').NextConfig} */
 const nextConfig = withPlaiceholder({
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src','styles')],
+  },
 })
 
 module.exports = nextConfig

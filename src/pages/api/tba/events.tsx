f3a66f7cf4ef3482879f804b58d63c9c33afac19
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2023-03-18 19:16:29 +1100

    fix events

--- src/pages/api/tba/events.tsx
+++ src/pages/api/tba/events.tsx
@@ -7,17 +7,20 @@ export default async function TBAEvents (req: NextApiRequest, res: NextApiRespon
 
   if (!tbaKey) return res.status(200).json([])
 
-  const response = await fetch(`https://www.thebluealliance.com/api/v3/team/frc3132/events/simple`, {
+  const request = await fetch(`https://www.thebluealliance.com/api/v3/team/frc3132/events/simple`, {
     headers: {
       "X-TBA-Auth-Key": tbaKey,
     }
-  }).then((res) => res.json())
+  })
+
+  const response = await request.json();
+
   res.setHeader(
     "Cache-Control",
     "public, s-maxage=1200, stale-while-revalidate=600"
   );
-  if (response.status === 200) {
-    const events = response.data
+  if (request.status === 200) {
+    const events = response
       .filter((event: any) => new Date(event.end_date).getTime() >= Date.now())
       .map((event: any) => ({
         city: event.city,
@@ -30,10 +33,10 @@ export default async function TBAEvents (req: NextApiRequest, res: NextApiRespon
         active: Date.now() >= new Date(event.start_date).getTime(),
       }));
 
-    res.status(response.status).json(events);
-  } else if (response.status === 304) {
+    res.status(request.status).json(events);
+  } else if (request.status === 304) {
     res.status(304);
-  } else if (response.status === 401) {
+  } else if (request.status === 401) {
     res.status(401);
   }
   res.end();

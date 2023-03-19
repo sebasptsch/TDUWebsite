aac568094a8b7a94445f67a2b8998c86a0c68e65
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-03-19 13:53:15 +1100

    flex grow

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,12 +3,14 @@ import Navigation from "./navigation";
 
 export default function Layout({ children }: any) {
   return (
-    <>
+    <div className="is-flex is-flex-direction-column" style={{
+      height: "100vh",
+    }}>
       <Navigation />
-      <section className="section">
+      <section style={{ flex: 1}} className="section">
         <div className="container">{children}</div>
       </section>
       <Footer />
-    </>
+    </div>
   );
 }

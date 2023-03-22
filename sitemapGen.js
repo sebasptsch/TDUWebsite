328e2ce744fb321471d1f4e1a9135cc1f8083b60
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2023-03-22 11:57:26 +1100

    fix response

--- sitemapGen.js
+++ sitemapGen.js
@@ -1,6 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || "https://www.team3132.com",
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-  };
-  
+  siteUrl: process.env.SITE_URL || "https://www.team3132.com",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
--- src/pages/api/instagram/feed.ts
+++ src/pages/api/instagram/feed.ts
@@ -1,17 +1,24 @@
 import { NextRequest } from "next/server";
 
-export default function handler(req: NextRequest) {
+export default async function handler(req: NextRequest) {
   const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN;
   if (!accessToken) {
     return new Response("No access token found", { status: 500 });
   }
-
+  // console.log("accessToken", accessToken)
   const url = `https://graph.instagram.com/2187701866/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=${accessToken}`;
 
-  return fetch(url, {
+  
+  const res = await fetch(url);
+
+  const data = await res.json();
+
+  console.log("data", data)
+
+  return new Response(JSON.stringify(data), {
     headers: {
-        "Content-Type": "application/json",
-        "Cache-Control": "public, s-maxage=12000, stale-while-revalidate=6000"
+      "Content-Type": "application/json",
+      "Cache-Control": "public, s-maxage=12000, stale-while-revalidate=6000"
     },
   });
 }

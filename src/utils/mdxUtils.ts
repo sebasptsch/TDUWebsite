083f978ee578dbc952d1c1f77aa73229c484a87e
Romain Charbonneau <romain.charbonneau@education.nsw.gov.au>
2025-01-25 14:25:57 +1100

    IndustryOutreach has been changed to Industry in the mdxUtils file

--- src/utils/mdxUtils.ts
+++ src/utils/mdxUtils.ts
@@ -5,7 +5,7 @@ import path from "path";
 export const OUTREACH_PATH = path.join(process.cwd(), "src", "data", "outreach");
 export const ROBOTS_PATH = path.join(process.cwd(), "src", "data", "robots");
 export const SPONSORS_PATH = path.join(process.cwd(), "src", "data", "sponsors.mdx");
-export const IndustryOutreach_PATH = path.join(process.cwd(), "src", "data", "IndusrtyOutreach");
+export const IndustryOutreach_PATH = path.join(process.cwd(), "src", "data", "Indusrty");
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
 export const outreachFilePaths = fs
